92789b021b6c627a9f6d26fc9c1946a87608d3ad
ThanhLuan-zaiko <deptrai05.pr@gmail.com>
2025-11-25 00:28:16 +0700

    home page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -39,6 +39,37 @@ public class HomeController : Controller
         return View(publishedTours);
     }
 
+    // GET: Home/TourDetails/5
+    public async Task<IActionResult> TourDetails(Guid? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var tour = await _context.Tours
+            .Include(t => t.Location)
+                .ThenInclude(l => l!.Country)
+            .Include(t => t.Category)
+            .Include(t => t.DefaultGuide)
+            .Include(t => t.Itineraries.OrderBy(i => i.DayIndex))
+            .Include(t => t.TourPrices)
+            .Include(t => t.TourInstances)
+                .ThenInclude(ti => ti.Guide)
+            .Include(t => t.TourServices)
+                .ThenInclude(ts => ts.Service)
+            .Include(t => t.TourImages.OrderBy(img => img.SortOrder))
+            .Where(t => !t.IsDeleted && t.IsPublished) // Only show published tours to public
+            .FirstOrDefaultAsync(m => m.TourID == id);
+        
+        if (tour == null)
+        {
+            return NotFound();
+        }
+
+        return View(tour);
+    }
+
     public IActionResult Privacy()
     {
         return View();
